8c9fb03f56f056523a031c757a42d76a1b086159
satspeedy <satspeedy@hotmail.com>
2015-11-18 22:59:44 +0100

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cattlecrewCaseManagementUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cattlecrewCaseManagementUiApp
+ */
+angular.module('cattlecrewCaseManagementUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
