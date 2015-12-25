12fca0328c8f1698d0ee53315ab8cdbb79360ce1
HHA <halil.hancioglu@opitz-consulting.com>
2015-12-25 20:21:44 +0100

    ui: adjusts code

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -13,13 +13,13 @@ angular.module('cattlecrewCaseManagementUiApp')
     camundaCaseService.getCaseDefinitions().then(function (result) {
       $scope.caseDefinitions = result.data;
     }, function(error) {
-      console.log('An error occured!', error);
+      console.log('An error occured during fetching case definitions!', error);
     });
 
     camundaCaseService.getCases().then(function (result) {
       $scope.cases = result.data;
     }, function(error) {
-      console.log('An error occured!', error);
+      console.log('An error occured during fetching cases!', error);
     });
 
   });
