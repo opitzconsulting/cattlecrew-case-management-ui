fa1cb3ea57c545359dea21bf6445a5cd8e5fe08f
HHA <halil.hancioglu@opitz-consulting.com>
2015-12-26 15:58:40 +0100

    ui: adjusts ordering dashboard table

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -8,18 +8,28 @@
  * Controller of the cattlecrewCaseManagementUiApp
  */
 angular.module('cattlecrewCaseManagementUiApp')
-  .controller('DashboardCtrl', function ($scope, camundaCaseService) {
+  .controller('DashboardCtrl', function($scope, camundaCaseService) {
 
-    camundaCaseService.getCaseDefinitions().then(function (result) {
-      $scope.caseDefinitions = result.data;
-    }, function(error) {
-      console.log('An error occured during fetching case definitions!', error);
-    });
+    $scope.initView = function() {
+      $scope.casesOrderBy = {
+        predicate: 'id',
+        reverse: true
+      };
 
-    camundaCaseService.getCases().then(function (result) {
-      $scope.cases = result.data;
-    }, function(error) {
-      console.log('An error occured during fetching cases!', error);
-    });
+      camundaCaseService.getCaseDefinitions().then(function(result) {
+        $scope.caseDefinitions = result.data;
+      }, function(error) {
+        console.log('An error occured during fetching case definitions!', error);
+      });
+
+      camundaCaseService.getCases().then(function(result) {
+        $scope.cases = result.data;
+      }, function(error) {
+        console.log('An error occured during fetching cases!', error);
+      });
+
+    };
+
+    $scope.initView();
 
   });
