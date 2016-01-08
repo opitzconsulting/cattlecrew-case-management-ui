5fe9599a53c77b292f5ebe6222996f2144c2aa97
till <tillothy@gmail.com>
2016-01-08 23:46:49 +0100

    milestones

--- app/scripts/controllers/casedetails.js
+++ app/scripts/controllers/casedetails.js
@@ -8,11 +8,18 @@
  * Controller of the cattlecrewCaseManagementUiApp
  */
 angular.module('cattlecrewCaseManagementUiApp')
-  .controller('CaseDetailsCtrl', function ($scope, $location, $routeParams, camundaCaseService) {
+  .controller('CaseDetailsCtrl', function ($scope, $location, $routeParams, camundaCaseService,
+				 camundaHistoryService, camundaMilestoneService, camundaActivityService) {
 
       var caseId = $routeParams.caseId;
+      $scope.history = camundaHistoryService.getHistoryById(caseId);
+      $scope.milestones = camundaMilestoneService.getMilestones(caseId);
+//    $scope.activities = camundaActivityService.getActivities(caseId);
+      var caseId = $routeParams.caseId;
+      console.log($scope.milestones);
       camundaCaseService.getCaseById(caseId).then(function (res) {
         $scope.case = res.data;
+	console.log($scope.case);
       }, function(error) {
         console.log('An error occured!', error);
       });
