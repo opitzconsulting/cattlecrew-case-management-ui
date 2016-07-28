4d37c3d13cba390b1dfe8592b575ab2c4d7bbf91
satspeedy <satspeedy@hotmail.com>
2016-07-28 20:14:53 +0200

    FIX grunt-war config

--- Gruntfile.js
+++ Gruntfile.js
@@ -449,7 +449,7 @@ module.exports = function (grunt) {
         files: [
           {
             expand: true,
-            cwd: 'build',
+            cwd: '<%= yeoman.dist %>',
             src: ['**'],
             dest: '.'
           }
